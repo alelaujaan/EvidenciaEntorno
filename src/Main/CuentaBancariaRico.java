01511da72a412fd443df3013be6e1d449a28b7b6
alelaujaan <alejandro.fernandezr@campusfp.es>
2023-03-21 11:09:17 +0100

    Ultimo Comit con las 3 pruebas

--- src/Main/CuentaBancariaRico.java
+++ src/Main/CuentaBancariaRico.java
@@ -7,11 +7,29 @@ import org.junit.jupiter.api.Test;
 class CuentaBancariaRico {
 
 	@Test
-	void test() throws Exception {
+	void test1() throws Exception {
+		CuentaBancaria test = new CuentaBancaria();
+		test.agregarSaldo(5000);
+		test.solicitarPrestamo(15000);
+		assertEquals(15000, test.obtenerPrestamo());
+	}
+	
+	
+	@Test
+	void test2() throws Exception{  
 		CuentaBancaria test = new CuentaBancaria();
 		test.agregarSaldo(2000);
 		test.solicitarPrestamo(12000);
 		assertEquals(6000, test.obtenerPrestamo());
+	}
+	
+	@Test
+	void test3(){
+		CuentaBancaria test = new CuentaBancaria();
+		
+		assertThrows(Exception.class, ()->test.solicitarPrestamo(2000));
+		assertThrows(Exception.class, ()->test.solicitarPrestamo(4000));
+		
 	}
 
 }
